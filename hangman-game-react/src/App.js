69a2452720331264f32b35e51ce615185fd6c73e
jenhuynh <jendoubleh@gmail.com>
2021-08-13 13:58:44 -0700

    Added counter with useState to create countdown timer

--- hangman-game-react/src/App.js
+++ hangman-game-react/src/App.js
@@ -5,7 +5,7 @@ import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 import Notification from './components/Notification';
 import Popup from './components/Popup';
-//because we can't have the same name as state when we import, we use as keyword to rename showNotication to show
+//because we can't have the same name as state when we import, we use as keyword to rename showNotication to the show name
 import { showNotification as show } from './helpers/helpers';
 import './App.css';
 
