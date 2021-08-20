5cf8eb544011b5a3e61619e4e99512cb9f97aed1
jenhuynh <jendoubleh@gmail.com>
2021-08-19 20:11:40 -0700

    Added edit button interactivity, ability to add form, and delete method for books

--- client/book-list.js
+++ client/book-list.js
@@ -1,10 +1,43 @@
 // holds the logic to updating/deleting books and displaying them on the page to retreive all books using browser-side JavaScript and our REST API
 const setEditModal = (isbn) => {
-    // We will implement this later
+     // Get information about the book using isbn, use the setEditModal method to gather information about the book when "Edit" button is clicked.
+    //  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+     const xhttp = new XMLHttpRequest();
+
+     xhttp.open("GET", `http://localhost:3000/book/${isbn}`, false);
+     xhttp.send();
+ 
+     const book = JSON.parse(xhttp.responseText);
+ 
+     const {
+         title,
+         author,
+         publisher,
+         publish_date,
+         numOfPages
+     } = book;
+ 
+     // Filling information about the book in the form inside the modal
+     document.getElementById('isbn').value = isbn;
+     document.getElementById('title').value = title;
+     document.getElementById('author').value = author;
+     document.getElementById('publisher').value = publisher;
+     document.getElementById('publish_date').value = publish_date;
+     document.getElementById('numOfPages').value = numOfPages;
+ 
+     // Setting up the action url for the book, the form's action parameter with the clicked book's URL to send the request:
+     document.getElementById('editForm').action = `http://localhost:3000/book/${isbn}`;
 }
 
+//implementing deletebook method to delete books by ISBN numbers
 const deleteBook = (isbn) => {
-    // We will implement this later
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("DELETE", `http://localhost:3000/book/${isbn}`, false);
+    xhttp.send();
+
+    // Reloading the page
+    location.reload();
 }
 
 const loadBooks = () => {
