d671518ec6257192fb93885910ab1c5c5e3c5a74
jenhuynh <jendoubleh@gmail.com>
2021-08-24 13:33:56 -0700

    Fixed sort state

--- week-7/todo-react-app/src/App.js
+++ week-7/todo-react-app/src/App.js
@@ -1,12 +1,13 @@
-
 import React from 'react';
 import Todo from './components/Todo';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, sort, settodos] = 
+  const [todos, settodos] = 
      useState ([{text: 'Walk dog', isCompleted: false, createdAt: new Date()}, {text: 'Feed cat', isCompleted: false, createdAt: new Date()}])
+
+  const [sort, setSort] = useState('oldest');
     
   return (
     <div className="App">
@@ -15,13 +16,10 @@ function App() {
         {/* todos.length checks that length is greater than 0, both conditions need to be true in order to run */}
           {todos.length &&
           //map is getting the key and props
-            todos.map((todo, idx) => <Todo key={idx} todo={todo} updateTodo={settodos}/>)
+            todos.map((todo, idx) => <Todo key={idx} todo={todo} updateTodo={settodos} sort={todo.sort}/>)
             }
-             <button
-    onClick={() => ({ sort: sort === 'oldest' ? 'newest' : 'oldest' })
-    }
-  >
-    ⬇️ Sort by {sort === 'oldest' ? 'newest' : 'oldest'} first
+             <button onClick={() => setSort('oldest')}>
+    ⬇️ Sort by {setSort === 'oldest' ? 'newest' : 'oldest'} first
   </button>
         </div>
       </div>
