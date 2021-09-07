518e72a0b9b56dc79cc42fd970969463cda0452d
jenhuynh <jendoubleh@gmail.com>
2021-09-07 11:42:29 -0700

    Moved users information into new component file

--- eventonica-react/src/App.js
+++ eventonica-react/src/App.js
@@ -1,108 +1,58 @@
 //React does not import images the same way as HTML because Babel converts the code into regular HTML and CSS for the browser to use, the path ends up being different
 import calendarImg from "./calendar.png";
 import "./App.css";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer";
+import Users from "./components/Users";
 
 //what is repeated that can be turned to reusable components: forms with input type as text, ul with list elements, submit button, fieldset  with label and input
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={calendarImg} alt="Calendar Star Logo" />
-        <h1>Eventonica</h1>
-      </header>
+    <>
+      <div className="App">
+        <header>
+          <img src={calendarImg} alt="Calendar Star Logo" />
+          <h1>Eventonica</h1>
+        </header>
 
-      <main>
-        <div className="user-and-events">
-          <section className="user-management">
-            <h2>User Management</h2>
-
-            <ul id="users-list">
-              {/* display all existing Users here */}
-              <li>...</li>
-            </ul>
-
-            <div>
-              <h3>Add User</h3>
-              <form id="add-user" action="#">
-                <fieldset>
-                  <label>Name</label>
-                  <input type="text" id="add-user-name" />
-                </fieldset>
-                {/* Add more form fields here */}
-                <input type="submit" value="Add" />
-              </form>
-            </div>
-
-            <div>
-              <h3>Delete User</h3>
-              <form id="delete-user" action="#">
-                <fieldset>
-                  <label>User ID</label>
-                  <input type="text" id="delete-user-id" />
-                </fieldset>
-                <input type="submit" />
-              </form>
-            </div>
-          </section>
-
-          <section className="event-management">
-            <h2>Event Management</h2>
-            <div>
-              <h3>All Events</h3>
-              <ul id="events-list">
-                {/* Display all Events here */}
-                <li>...</li>
-              </ul>
-
-              <h3>Add Event</h3>
-              <form id="add-event" action="#">
-                <fieldset>
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    id="add-event-name"
-                    placeholder="Virtual corgi meetup"
-                  />
-                </fieldset>
-                {/* Add more form fields here */}
-                <input type="submit" />
-              </form>
-            </div>
-          </section>
-        </div>
-
-        <div>
-          <h3>Delete Event</h3>
-          <form id="delete-event" action="#">
-            <fieldset>
-              <label>Event ID</label>
-              <input type="number" min="1" id="delete-event-id" />
-            </fieldset>
-            <input type="submit" />
-          </form>
-        </div>
-
-        <aside className="search-toolbar">
+        <main>
+          <div className="user-and-events"></div>
+          <section>{Users}</section>
           <div>
-            <h3>Find Events</h3>
-            <form id="search" action="#">
+            <h3>Delete Event</h3>
+            <form id="delete-event" action="#">
               <fieldset>
-                <label htmlFor="date-search">Date</label>
-                <input type="text" id="date-search" placeholder="YYYY-MM-DD" />
+                <label>Event ID</label>
+                <input type="number" min="1" id="delete-event-id" />
               </fieldset>
-              <fieldset>
-                <label htmlFor="category-search">Category</label>
-                <input type="text" id="category-search" />
-              </fieldset>
-
-              <input type="submit" value="Search" />
+              <input type="submit" />
             </form>
           </div>
-        </aside>
-      </main>
-      <footer>{Footer}</footer>
-    </div>
+
+          <aside className="search-toolbar">
+            <div>
+              <h3>Find Events</h3>
+              <form id="search" action="#">
+                <fieldset>
+                  <label htmlFor="date-search">Date</label>
+                  <input
+                    type="text"
+                    id="date-search"
+                    placeholder="YYYY-MM-DD"
+                  />
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="category-search">Category</label>
+                  <input type="text" id="category-search" />
+                </fieldset>
+
+                <input type="submit" value="Search" />
+              </form>
+            </div>
+          </aside>
+        </main>
+        <footer>{Footer}</footer>
+      </div>
+    </>
   );
 }
 
