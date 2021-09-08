3e17cadaca56fa5efb55e8134092e62f0cec8905
jenhuynh <jendoubleh@gmail.com>
2021-09-08 14:10:32 -0700

    add useReducer functionality

--- eventonica-react/src/components/Events.jsx
+++ eventonica-react/src/components/Events.jsx
@@ -1,4 +1,38 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
+
+  //initial state for the form reducer which is an object with keys for each field in the form, values will be updated as the user fills out the form
+  const initialState = {
+    id: "",
+    name: "",
+    date: null,
+    description: "",
+    category: "",
+  };
+
+       //reducer function, each action type will mutate the state in its own way and reducer function returns a new state
+       const reducer = (state, action) => {
+        console.log(action, "this is the action");
+        
+        switch (action.type) {
+          case "editName":
+            console.log("Logged if the editName action is being dispatched")
+            return { ...state, name: action.payload };
+            // payload refers to the data of the state
+        case "editId":
+            return { ...state, id: action.payload };
+        case "editDate":
+                return { ...state, date: action.payload };
+          case "editDescription":
+            return { ...state, description: action.payload };
+      
+          case "editCategory":
+            return { ...state, category: action.payload };
+      
+          default:
+            return state;
+        }
+      };
+
 //create UI that renders a list of events
 const Events = () => {
     const event1 = {
@@ -24,33 +58,50 @@ const Events = () => {
         description: "A chance to practice Javascript interview questions",
         category: "Education",
       };
-
-      //creating events state to initilize with mock events
-      const [events, setEvents] = useState([event1, event2, event3]);
-
       //use setstate to id field
-      const [id, setId] = useState("");
+    //   const [id, setId] = useState("");
     
-      //use setstate to name field
-      const [name, setName] = useState("");
+    //   //use setstate to name field
+    //   const [name, setName] = useState("");
 
-      //use setstate to date field
-      const [date, setDate] = useState("");
+    //   //use setstate to date field
+    //   const [date, setDate] = useState("");
 
-      //use setstate to description field
-      const [description, setDescription] = useState("");
+    //   //use setstate to description field
+    //   const [description, setDescription] = useState("");
 
-       //use setstate to category field
-       const [category, setCategory] = useState("");
+    //    //use setstate to category field
+    //    const [category, setCategory] = useState("");
 
-    //add onsubmit function for form
-    const onSubmit = (e) => {
-        e.preventDefault(); 
-        const newEvent = {id, name, date, description, category};
-        const newEventList = [...events, newEvent ];
-        debugger;
-        setEvents(newEventList);
-    }
+
+    // const EventForm = () => {
+         //creating events state to initilize with mock events
+      const [events, setEvents] =  useState([event1, event2, event3]  );
+    //   const [events, setEvents] =  useState([event1, event2, event3]);
+      //reducers will store and update the form data
+        const [state, dispatch] = useReducer(reducer, initialState);
+          //add onsubmit function for form
+    // const onSubmit = (e) => {
+    //     e.preventDefault(); 
+    //     // const newEvent = {id, name, date, description, category};
+    //     const newEvent = {editId editName; editDate, editDescription, editCategory};
+    //     const newEventList = [...events, newEvent ];
+    //     setEvents(state);
+    // }
+    const addEvent = e => {
+        e.preventDefault();
+        const newEvent = {
+            //object named state but it does not have separate data 
+          id: state.id,
+          name: state.name,
+          date: state.date,
+          description: state.description,
+          category: state.category,
+        }
+        setEvents([...events, newEvent])
+        console.log(newEvent)
+      }
+ 
     return (
         <>
         <section className="event-management">
@@ -65,17 +116,25 @@ const Events = () => {
           </ul>
 
           <h3>Add Event</h3>
-          <form id="add-event" action="#" onSubmit={onSubmit}>
+          <form id="add-event" action="#">
             <fieldset>
             {/* event id field */}
             <div>
             <label>Event Id</label>
             <input
                 type="text"
+                required
+                name="id"
                 id="add-event-id"
-                value={id}
+                value={state.id}
                 placeholder="Add event id"
-                onChange={(e) => setId(e.target.value)}
+                onChange={(e) =>
+                    dispatch({
+                    type: "editId",
+                    payload: e.target.value
+                    })
+                  }
+                // onChange={(e) => setId(e.target.value)}
               />
               </div>
               
@@ -84,10 +143,17 @@ const Events = () => {
               <label>Name</label>
               <input
                 type="text"
+                name="name"
                 id="add-event-name"
-                value={name}
+                value={state.name}
                 placeholder="Add event name"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) =>
+                    dispatch({
+                    type: "editName",
+                    payload: e.target.value
+                    })
+                  }
+                // onChange={(e) => setName(e.target.value)}
               />
               </div>
                {/* date field */}
@@ -95,10 +161,17 @@ const Events = () => {
             <label>Date</label>
             <input
                 type="date"
+                name="date"
                 id="add-event-date"
-                value={date}
+                value={state.date}
                 placeholder="Add event date"
-                onChange={(e) => setDate(e.target.value)}
+                onChange={(e) =>
+                    dispatch({
+                    type: "editDate",
+                    payload: e.target.value
+                    })
+                  }
+                // onChange={(e) => setDate(e.target.value)}
               />
               </div>       
               {/* description field */}
@@ -106,10 +179,17 @@ const Events = () => {
             <label>Description</label>
             <input
                 type="text"
+                name="description"
                 id="add-event-description"
-                value={description}
+                value={state.description}
                 placeholder="Add event description"
-                onChange={(e) => setDescription(e.target.value)}
+                onChange={(e) =>
+                    dispatch({
+                    type: "editDescription",
+                    payload: e.target.value
+                    })
+                  }
+                // onChange={(e) => setDescription(e.target.value)}
               />
               </div>  
                 {/* category field */}
@@ -117,22 +197,28 @@ const Events = () => {
             <label>Category</label>
             <input
                 type="text"
+                name="category"
                 id="add-event-category"
-                value={category}
+                value={state.category}
                 placeholder="Add event category"
-                onChange={(e) => setCategory(e.target.value)}
+                onChange={(e) =>
+                    dispatch({
+                    type: "editCategory",
+                    payload: e.target.value
+                    })
+                  }
+                // onChange={(e) => setCategory(e.target.value)}
               />
               </div>  
-            
             </fieldset>
-            
-            <input type="submit" />
+            <input type="submit"  onClick={addEvent}/>
           </form>
-    
         </div>
       </section>
+
     </>
   )
 }
+
 
 export default Events;
