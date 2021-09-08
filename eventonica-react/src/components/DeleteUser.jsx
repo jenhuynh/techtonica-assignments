c655cfc1b93a07cc7686a63b806bd7d95aef7cc4
jenhuynh <jendoubleh@gmail.com>
2021-09-07 18:44:15 -0700

    Fix delted users functionality

---
+++ eventonica-react/src/components/DeleteUser.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Users  from "./Users";
+
+function DeleteUser ({deleteUser}) {
+    //add state to store what deleteID the user has typed and change delete user
+    const [deleteId, setDeleteId] = useState("");
+
+  
+    //add obSubmit function
+    const onSubmit = (event) => {
+        event.preventDefault(); 
+        //callback function to delete user ids
+        deleteUser(deleteId);
+        setDeleteId("");
+    }
+    return(
+        <>
+        <div>
+        <h3>Delete User</h3>
+        <form id="delete-user" action="#" onSubmit={onSubmit}>
+            <fieldset>
+            <label>User ID</label>
+            <input type="text" 
+            id="delete-user-id" 
+            value={deleteId} 
+            onChange={(e) => setDeleteId(e.target.value)}/>
+            </fieldset>
+            <input type="submit" value="Delete" />
+        </form>
+        </div>
+        </>
+    )
+}
+
+export default DeleteUser;
