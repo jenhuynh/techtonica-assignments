f7b9dcc96b6db2a85d8341eae5bc2c459d3a6416
jenhuynh <jendoubleh@gmail.com>
2021-09-08 20:42:37 -0700

    Add cors

--- eventonica-api/app.js
+++ eventonica-api/app.js
@@ -3,7 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+//require CORS
+var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 //add events route
@@ -19,7 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+//have express use CORS
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 //add events route
