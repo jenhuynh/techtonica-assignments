0205cb67aaea2b0af847d45964353166f7399577
jenhuynh <jendoubleh@gmail.com>
2021-09-09 16:06:57 -0700

    Added delete users route

--- eventonica-api/routes/users.js
+++ eventonica-api/routes/users.js
@@ -2,34 +2,44 @@ var express = require("express");
 var router = express.Router();
 
 //mock users
-const users = [
-  { name: "Marlin", email: "marlin@gmail.com", id: "1" },
-  { name: "Nemo", email: "nemo@gmail.com", id: "2" },
-  { name: "Dory", email: "dory@gmail.com", id: "3" },
+let USERS = [
+  { name: "Marlin", email: "marlin@gmail.com", id: 1 },
+  { name: "Nemo", email: "nemo@gmail.com", id: 2 },
+  { name: "Dory", email: "dory@gmail.com", id: 3 },
 ];
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.json(users);
+  res.json(USERS);
 });
 
-router.post("/", function (req, res) {
+router.post("/add", function (req, res) {
   const newUser = req.body;
-  users.push(newUser);
+  USERS.push(newUser);
   res.json(newUser);
 });
 
 // Delete a User
-router.delete("/", function (req, res) {
-  const id = req.params.id;
-  console.log();
+router.post("/delete/:id", function (req, res) {
+  console.log("DELETE REQUEST");
+  const id = Number(req.params.id);
+  console.log(id);
   // remove item by id
   // you are automatically updating users w/ filter
-  users = users.filter((user) => {
+  console.log(USERS);
+  const xy = USERS.filter((user) => {
+    console.log(user.id, id, user.id !== id);
     // only return things that don't have the id
     user.id !== id;
   });
-  res.json(user);
+  USERS = USERS.filter((user) => {
+    console.log(user.id, id, user.id !== id);
+    // only return things that don't have the id
+    user.id !== id;
+  });
+  console.log(xy, USERS);
+  //my response please set the http 204 which means no content and .end sends the response
+  res.status(204).end();
 });
 
 module.exports = router;
