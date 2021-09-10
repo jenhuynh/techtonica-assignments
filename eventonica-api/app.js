cfdff58d364f9eec2dbcf5cd412ffc04d9ba06d9
jenhuynh <jendoubleh@gmail.com>
2021-09-10 14:01:41 -0700

    Comment out cors, delete it

--- eventonica-api/app.js
+++ eventonica-api/app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 //require CORS
-var cors = require("cors");
+// var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 //add events route
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 //have express use CORS
-app.use(cors());
+// app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 //add events route
