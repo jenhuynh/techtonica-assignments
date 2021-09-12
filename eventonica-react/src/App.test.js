7e92095bd9ee334d56158b8ee9d507e930119aec
jenhuynh <jendoubleh@gmail.com>
2021-09-12 14:52:03 -0700

    Add app.test test cases

--- eventonica-react/src/App.test.js
+++ eventonica-react/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+//testing if app component exists and renders
+//screen.debug will show us the result of the redner()call
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+    screen.debug();
+  });
 });
